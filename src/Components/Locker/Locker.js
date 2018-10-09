8a79d49a6b7014855bb3087126295126d2bcb952
Jake <jmager12@gmail.com>
2018-10-08 23:04:10 -0400

    Locker Add/Remove from cart

--- src/Components/Locker/Locker.js
+++ src/Components/Locker/Locker.js
@@ -12,7 +12,9 @@ export default class Locker extends Component {
 		this.state = {
 			loading: true,
 			items: [],
-			filteredItems: []
+			filteredItems: [],
+			cartItems: [],
+			hoveredItem: null
 		};
 	}
 
@@ -24,6 +26,7 @@ export default class Locker extends Component {
 		const { filter } = this.props;
 		const { items } = this.state;
 
+		// If user does search
 		if (filter !== prevProps.filter) {
 			let filteredItems = [...items];
 			if (filter.length) {
@@ -31,7 +34,6 @@ export default class Locker extends Component {
 					item.name.toLowerCase().includes(filter.toLowerCase())
 				);
 			}
-
 			this.setState({ filteredItems });
 		}
 	}
@@ -41,22 +43,72 @@ export default class Locker extends Component {
 			method: 'post',
 			url: 'http://localhost:8888/server/locker/getItems.php'
 		}).then(res => {
-			console.log(res.data);
 			this.setState({ items: res.data, filteredItems: res.data, loading: false });
 		});
 	};
 
+	addToCart = item => {
+		const { cartItems } = this.state;
+		this.setState({ cartItems: [...cartItems, item] });
+	};
+
+	removeFromCart = itemToRemove => {
+		const { cartItems } = this.state;
+		let newCartItems = cartItems.filter(item => item !== itemToRemove);
+		this.setState({ cartItems: newCartItems });
+	};
+
 	renderItems = () => {
-		const { filteredItems } = this.state;
+		const { filteredItems, hoveredItem, cartItems } = this.state;
 
 		return filteredItems.map(item => {
+			let inCart = cartItems.includes(item);
+
 			return (
-				<div key={item.Id} className="itemContainer">
-					<div className="itemImageContainer">
-						<img src={`${item.image}`} alt={item.name} className="itemImage" />
+				<div
+					key={item.Id}
+					className="itemContainer"
+					onMouseEnter={() => this.setState({ hoveredItem: item.Id })}
+					onMouseLeave={() => this.setState({ hoveredItem: null })}
+				>
+					{inCart ? (
+						<div className="itemImageContainer itemImageContainerOverlay">
+							<div className="inCartOverlay itemImage">
+								<i className="inCartIcon fa fa-check" />
+								<img src={`${item.image}`} alt={item.name} className="itemImage" />
+							</div>
+						</div>
+					) : (
+						<div className="itemImageContainer">
+							<img src={`${item.image}`} alt={item.name} className="itemImage" />
+						</div>
+					)}
+
+					<div
+						style={
+							inCart
+								? { backgroundColor: '#97db8f', height: '100%', zIndex: '4' }
+								: { height: '100%' }
+						}
+					>
+						<div className="itemTitle">{item.name}</div>
+						<div className="itemAvailability">{item.quantityAvailable} Available</div>
+						{hoveredItem === item.Id ? (
+							<div className="itemIconsContainer">
+								<i className="itemIcon info fa fa-info" />
+								{inCart ? (
+									<i
+										onClick={() => this.removeFromCart(item)}
+										className="itemIcon minus fa fa-minus"
+									/>
+								) : (
+									<i onClick={() => this.addToCart(item)} className="itemIcon plus fa fa-plus" />
+								)}
+							</div>
+						) : (
+							<span />
+						)}
 					</div>
-					<div className="itemTitle">{item.name}</div>
-					<div className="itemAvailability">{item.quantityAvailable} Available</div>
 				</div>
 			);
 		});
